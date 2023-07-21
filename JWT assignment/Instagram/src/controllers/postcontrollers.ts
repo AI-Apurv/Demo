5119d583f7b1bb23a482f946826ab9becfc58343
AI-Apurv <137977357+AI-Apurv@users.noreply.github.com>
2023-07-21 12:46:37 +0530

    JWT assignment

--- JWT assignment/Instagram/src/controllers/postcontrollers.ts
+++ JWT assignment/Instagram/src/controllers/postcontrollers.ts
@@ -9,22 +9,24 @@ export async function createPost(req: Request, res: Response) {
   try {
     const { caption, post_url } = req.body;
     const token = req.header('Authorization')?.split(' ')[1];
+    console.log(token)
 
     if (!token) {
       return res.status(401).json({ message: 'Access denied. Token missing.' });
     }
 
     // Verify the token and extract user information from it
-    const decodedToken = jwt.verify(token, JWT_SECRET_KEY,) as { user_name: string , email: string };
-    const { user_name, email } = decodedToken;
-
+    const decodedToken = jwt.verify(token, 'secretKey')
+    console.log(decodedToken)
+    const  user_name  = decodedToken;
+  
    
-    PostModel.findOne({email})
-        .then((email : any)=> {
-          if(email){
-            return res.status(500).json({error : 'Post already exists'})
-          }
-        })
+    // Check if a post with the same post_url already exists
+    const existingPost = await PostModel.findOne({ post_url });
+
+    if (existingPost) {
+      return res.status(409).json({ error: 'Post with the same URL already exists' });
+    }
 
     // Create a new post using the Post model and save it to the database
     const newPost = new PostModel({
@@ -34,7 +36,7 @@ export async function createPost(req: Request, res: Response) {
     });
     await newPost.save();
 
-    res.status(201).json({ message: 'Post created successfully', createdBy: { user_name, email }, post: newPost });
+    res.status(201).json({ message: 'Post created successfully', createdBy: { user_name }, post: newPost });
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' });
   }
